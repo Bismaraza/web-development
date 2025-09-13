9ec4fec8d17af296678eacecfbff046dba7febbc
Bisma Raza <bismauniversity@gmail.com>
2025-09-13 15:56:08 +0500

    Java Questions

--- Java Code/hello.java
+++ Java Code/hello.java
@@ -1,17 +1,34 @@
+// import java.util.*;
+
+// public class hello {
+
+//     public static void main(String[] args) {
+//         // calculating sum of two numbers
+//         int a = 4;
+//         int b = 5;
+//         int sum = a + b;
+//         System.out.println(sum);
+
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your name");
+//         String name = sc.nextLine();
+//         System.out.println("Hello " + name);
+
+//     }
+// }
+
 import java.util.*;
-
 public class hello {
-
     public static void main(String[] args) {
-        // calculating sum of two numbers
-        int a = 4;
-        int b = 5;
-        int sum = a + b;
-        System.out.println(sum);
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name");
-        String name = sc.nextLine();
-        System.out.println("Hello " + name);
+//         Scanner scan = new Scanner(System.in);
+//         float a = scan.nextFloat();
+// float area = 3.14f * a * a;
+//         System.out.println(area);
 
+//TYpe Casting
+        float a = 25.99f;
+        int b = (int)a ;
+        System.out.println(b);
     }
 }
