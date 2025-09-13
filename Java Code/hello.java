7ce020ff5b3f216c33cd77905f1f4256cac0713e
Bisma Raza <bismauniversity@gmail.com>
2025-09-13 11:24:15 +0500

    Java

---
+++ Java Code/hello.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class hello {
+
+    public static void main(String[] args) {
+        // calculating sum of two numbers
+        int a = 4;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+
+    }
+}
